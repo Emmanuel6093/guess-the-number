e9236c8b401bce6d77f1ae399b3b3126f9889917
Emmanuel6093 <elorenzo6093@gmail.com>
2024-05-28 20:38:51 -0400

    entry point of game added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace GuessTheNumberGame
     {
         static void Main(string[] args)
         {
+            Random random = new Random(); 
+            int correctNumber = random.Next(1, 101); 
+            int guess = 0;
 
+            Console.WriteLine("Welcome to the Guess the Number Game!");
+            Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess what it is?");
         }
     }
 }
